db097807d126385aa70fdf515b2d13cbfc58c172
programeri <programeri@ucione.local>
2017-05-03 20:36:53 +0200

    Update View-a welcome i show, dodan view index

---
+++ resources/views/items/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang=hr>
+    <head>
+		<meta charset="utf-8">
+		<title>Algebra blog</title>
+    </head>
+    <body>
+		@foreach ($items as $item)
+			<h2><a href="{{ $item->id }}">{{ $item->title }} </a></h2>
+			<div>{{ $item->content }} </div>
+		@endforeach
+	</body>
+</html>
